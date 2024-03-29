1644b8cd8d50ab199401e8517fbaa68fab2cf9f7
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-03-29 10:23:48 -0400

    Bubble sort

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,27 @@
+// Problem :- Given an array of integers, sort the array. 
+
+// const arr = [-6, 20, 8, -2, 4]
+// [-6, 20, 8, -2, 4, 57, 1, 3, 11, 7]
+
+// bubbleSort(arr) => should be [-6, -2, 4, 8, 20]
+
+// function bubbleSort(arr) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (let i = 0; i < arr.length - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped)
+// }
+
+// const arr = [-6, 20, 8, -2, 4, 57, 1, 3, 11, 7]
+// bubbleSort(arr)
+// console.log(arr)
+
+
