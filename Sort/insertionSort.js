c8fefead86136efe29d8255cad8a7aeab7ec7a73
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-03-29 10:23:57 -0400

    Insertion sort

---
+++ Sort/insertionSort.js
@@ -0,0 +1,19 @@
+// Problem :- Given an array of integers, sort the array 
+
+// const arr = [-6,20,8,-2,4] 
+
+function insertionSort(arr) {
+    for(let i=1; i<arr.length; i++){
+        let numberToInsert = arr[i]
+        let j = i - 1
+        while (j >= 0 && arr[j] > numberToInsert) {
+           arr[j+1] = arr[j]
+            j = j-1
+        }
+        arr[j+1] = numberToInsert
+    }
+}
+
+const arr = [-6,20,8,-2,4]
+insertionSort(arr)
+console.log(arr)
